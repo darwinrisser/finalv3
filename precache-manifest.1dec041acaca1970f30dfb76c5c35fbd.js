9bf57c4c10b7788f1de4a642cece837920e10575
Darwin <risser.darwin@gmail.com>
2019-04-23 20:35:50 -0500

    Updates

---
+++ precache-manifest.1dec041acaca1970f30dfb76c5c35fbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39316938ac04bcf54170",
+    "url": "/teacherr/static/js/runtime~main.c7fcd6d8.js"
+  },
+  {
+    "revision": "392ba4d7c9e8b81ca942",
+    "url": "/teacherr/static/js/main.037f621b.chunk.js"
+  },
+  {
+    "revision": "f432bcd8a6177ec06978",
+    "url": "/teacherr/static/js/2.d1b3fbd9.chunk.js"
+  },
+  {
+    "revision": "392ba4d7c9e8b81ca942",
+    "url": "/teacherr/static/css/main.5b514164.chunk.css"
+  },
+  {
+    "revision": "f2edeeeb43cc46f4dced335c8a836121",
+    "url": "/teacherr/index.html"
+  }
+];
