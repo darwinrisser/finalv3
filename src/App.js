061561735639da042559b7acdda2b3ee2afa65a5
Darwin <risser.darwin@gmail.com>
2019-04-26 18:23:05 -0500

    friday immits

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={'/teacherr'}>
           <MenuAppBar/>
           <Route exact path='/' component={Home}/>
           <Route exact path='/signin' component={SignIn}/>
